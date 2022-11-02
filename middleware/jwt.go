f6df7864ea5d775e471db16ebec16eb5a9393844
chyuhung <977599667@qq.com>
2022-11-02 16:49:31 +0800

    jwt

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -2,11 +2,16 @@ package middleware
 
 import (
 	"ginblog/utils"
+	"ginblog/utils/errmsg"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
+	"net/http"
+	"strings"
 	"time"
 )
 
 var JwtKey = []byte(utils.JwtKey)
+var code int
 
 type MyClaims struct {
 	Username string `json:"username"`
@@ -26,8 +31,51 @@ func SetToken(username string, password string) (string, int) {
 		},
 	}
 	reqClaim := jwt.NewWithClaims(jwt.SigningMethodES256, SetClaims)
+	token, err := reqClaim.SignedString(JwtKey)
+	if err != nil {
+		return "", errmsg.ERROR
+	}
+	return token, errmsg.SUCCSE
 }
 
 // 验证token
+func checkToken(tokenString string) (*MyClaims, int) {
+	token, _ := jwt.ParseWithClaims(tokenString, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return JwtKey, nil
+	})
+	if claims, ok := token.Claims.(*MyClaims); ok && token.Valid {
+		return claims, errmsg.SUCCSE
+	} else {
+		return nil, errmsg.ERROR
+	}
+}
 
-// jwt中间件
+// JwtToken jwt中间件
+func JwtToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenHeader := c.Request.Header.Get("Authorization")
+		if tokenHeader == "" {
+			code = errmsg.ERROR_TOKEN_EXIST
+		}
+		userToken := strings.SplitN(tokenHeader, " ", 2)
+		if len(userToken) != 2 && userToken[0] != "Bearer" {
+			code = errmsg.ERROR_TOKEN_TYPE_WRONG
+			c.Abort()
+		}
+		key, Tcode := checkToken(userToken[1])
+		if Tcode == errmsg.ERROR {
+			code = errmsg.ERROR_TOKEN_WRONG
+			c.Abort()
+		}
+		if time.Now().Unix() > key.ExpiresAt {
+			code = errmsg.ERROR_TOKEN_RUNTIME
+			c.Abort()
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"code":    code,
+			"message": errmsg.GetErrMsg(code),
+		})
+		c.Set("username", key.Username)
+		c.Next()
+	}
+}
