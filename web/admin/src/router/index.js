23ecf72e8b4afed26f9f7a67222fbc2e18cd3a0c
chyuhung <977599667@qq.com>
2023-07-25 11:33:02 +0800

    完成前端框架基础配置

--- web/admin/src/router/index.js
+++ web/admin/src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login'
+import Admin from '../views/Admin'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
   }
 ]
 
