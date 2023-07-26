fe9e1a954de5dd0aed8eff6148625564ce048590
chyuhung <977599667@qq.com>
2023-07-26 14:53:16 +0800

    更新一些包

--- web/admin/src/main.js
+++ web/admin/src/main.js
@@ -4,8 +4,9 @@ import router from './router'
 import axios from 'axios'
 import './plugin/antui'
 
+Vue.prototype.$axios = axios
+
 axios.defaults.baseURL = 'http://localhost:3000/api/v1'
-Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
