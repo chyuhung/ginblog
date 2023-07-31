23ff7487123f23cb4aeca7d9078525c059c37f7e
chyuhung <977599667@qq.com>
2023-07-31 16:25:49 +0800

    样式调整正常

--- web/admin/src/main.js
+++ web/admin/src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+
 import './plugin/antui'
+import './assets/css/style.css'
+import 'ant-design-vue/dist/antd.css'
 
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://localhost:3000/api/v1'
--- web/admin/src/views/Login.vue
+++ web/admin/src/views/Login.vue
@@ -1,6 +1,53 @@
 <template>
-  <div>
-    登录页面
-    <a-button type="primary"> 登录 </a-button>
+  <div class="container">
+    <div class="loginBox">
+      <a-form-model class="loginForm">
+        <a-form-model-item>
+          <a-input placeholder="请输入用户名">
+            <a-icon
+              slot="prefix"
+              type="user"
+              style="color: rgba(0, 0, 0, 0.25)"
+          /></a-input>
+        </a-form-model-item>
+        <a-form-model-item>
+          <a-input placeholder="请输入密码"
+            ><a-icon
+              slot="prefix"
+              type="lock"
+              style="color: rgba(0, 0, 0, 0.25)"
+          /></a-input>
+        </a-form-model-item>
+        <a-form-model-item class="loginBtn">
+          <a-button type="primary" style="margin: 10px">登录</a-button>
+          <a-button type="info" style="margin: 10px">取消</a-button>
+        </a-form-model-item>
+      </a-form-model>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  height: 100%;
+  background-color: rgb(75, 78, 106);
+}
+.loginBox {
+  width: 450px;
+  height: 300px;
+  background-color: rgb(255, 255, 255);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 9px;
+}
+
+.loginForm {
+  width: 100%;
+  position: absolute;
+  bottom: 10%;
+  padding: 0% 20%;
+  box-sizing: border-box;
+}
+</style>
