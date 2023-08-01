93da565410430a6ffedd96db2dc3037e7d578ae9
chyuhung <977599667@qq.com>
2023-08-01 15:45:53 +0800

    前端

--- web/admin/src/views/Login.vue
+++ web/admin/src/views/Login.vue
@@ -1,21 +1,15 @@
 <template>
   <div class="container">
     <div class="loginBox">
-      <a-form-model class="loginForm">
-        <a-form-model-item>
-          <a-input placeholder="请输入用户名">
-            <a-icon
-              slot="prefix"
-              type="user"
-              style="color: rgba(0, 0, 0, 0.25)"
+      <a-form-model :rules="rules" :model="formdata" class="loginForm">
+        <a-form-model-item prop="username">
+          <a-input v-model="formdata.username" placeholder="用户名">
+            <a-icon slot="prefix" type="user" style="color: rgba(0, 0, 0, 0.25)"
           /></a-input>
         </a-form-model-item>
-        <a-form-model-item>
-          <a-input placeholder="请输入密码"
-            ><a-icon
-              slot="prefix"
-              type="lock"
-              style="color: rgba(0, 0, 0, 0.25)"
+        <a-form-model-item prop="password">
+          <a-input v-model="formdata.password" placeholder="密码" type="password">
+            <a-icon slot="prefix" type="lock" style="color: rgba(0, 0, 0, 0.25)"
           /></a-input>
         </a-form-model-item>
         <a-form-model-item class="loginBtn">
@@ -26,6 +20,44 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 4, max: 12, message: '用户名必须在4到12个字符之间', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 20, message: '密码必须在6到20个字符之间', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.ruleForm.validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm() {
+      this.$refs.ruleForm.resetFields()
+    }
+  }
+}
+</script>
 
 <style scoped>
 .container {
