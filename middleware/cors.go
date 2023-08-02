f81757ce7b45bacfd4480eecb613dfde73a41262
chyuhung <977599667@qq.com>
2023-08-02 17:11:48 +0800

    修改cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -11,13 +11,12 @@ import (
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		cors.New(cors.Config{
-			// AllowAllOrigins:        true,
-			AllowOrigins: []string{"*"},
-			AllowMethods: []string{"*"},
-			AllowHeaders: []string{"Origin", "Content-Type", "Access-Control-Allow-Origin"},
-			// AllowCredentials: true,
-			ExposeHeaders: []string{"Content-Length", "Authorization"},
-			MaxAge:        12 * time.Hour,
+			AllowAllOrigins:  true,
+			AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowHeaders:     []string{"*"},
+			ExposeHeaders:    []string{"Content-Length", "Authorization", "Content-Type"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
 		})
 	}
 }
